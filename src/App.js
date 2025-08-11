c8f2da151f76f95407c1264c5194bcb204e653fc
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2025-08-11 22:49:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+import the_river_between_us from "./The_River_Between_Us__by_Ravin_Kumar.pdf"
+import aakhiri_himmat from "./Aakhiri_Himmat.pdf"
+function App() {
+  return (
+    <div>
+    <center><h1><b><i> Ravin Writings </i></b></h1></center>
+   <br/>
+   <div class="middle">
+     <ol>
+    <li>
+    <b><u><a href={the_river_between_us} target="_blank" rel="noopener noreferrer" style={{color:"black"}}>The River Between Us: Because Even Rivers Choose Love</a></u></b>.<br/>
+    
+     <p align="justify">
+The River Between Us is a poetic and heartwarming romantic short story by <a href="https://mr-ravin.github.io" target="_blank" rel="noopener noreferrer">Ravin Kumar</a>, exploring the quiet power of childhood connection, fate, and timeless love. Set across a serene village and a modern cruise ship, the story follows Ravin and Pooja—two soulmates bound by paper boats, a copper ring, and a mysterious letter “R”. As life separates them and time flows on like a river, a quiet reunion unfolds through symbols, silence, and destiny.
+
+This story blends memory, emotion, and subtle symbolism to remind us that true love never forgets—and always finds its way home.
+     </p>
+
+
+     <p align="justify">
+     <b><i>Available on</i></b>:
+     
+     <b> 1. </b> <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/books/details?id=zgZ0EQAAQBAJ">Google Play</a>
+     <b> 2. </b> <a target="_blank" rel="noopener noreferrer" href="https://books.google.co.in/books/about?id=zgZ0EQAAQBAJ">Google Books</a>
+     <b> 3. </b> <a target="_blank" rel="noopener noreferrer" href="https://mr-ravin.github.io/The_River_Between_Us__by_Ravin_Kumar.pdf">Github</a>
+     <b> 4. </b> <a target="_blank" rel="noopener noreferrer" href="https://archive.org/details/the-river-between-us--by-ravin-kumar">Archive.org</a>
+     <b> 5. </b> <a target="_blank" rel="noopener noreferrer" href="https://www.scribd.com/document/892078204/The-River-Between-Us-By-Ravin-Kumar">Scribd</a>
+     <b> 6. </b> <a target="_blank" rel="noopener noreferrer" href="https://leanpub.com/the_river_between_us__by_ravin_kumar">Leanpub</a>
+     <b> 7. </b><a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ch.ravinkumar/the-river-between-us-because-even-rivers-choose-love-70d73ceff427">Medium</a>
+     </p>
+
+    </li>
+    <br/><br/>
+    <li>
+    <b><u><a href={aakhiri_himmat} target="_blank" rel="noopener noreferrer" style={{color:"black"}}>आख़िरी हिम्मत (Aakhiri Himmat)</a></u></b>.<br/>
+— रविन कुमार<br/>
+     <p align="justify">
+
+      ऐ मायूस इंसान, बस एक आख़िरी हिम्मत जुटा के देख,<br/>
+
+      खुद की नज़रों में खुदी को, खुद का ख़ुदा बना के देख।<br/>
+
+      तेरा समंदर तू, तेरा आस्माँ तू,<br/>
+
+      तेरा दरिया तू, तेरा ज़रिया तू।<br/>
+
+      कर इबादत तू खुद से खुदी की,<br/>
+
+      होगी हासिल जो भी तेरी मंज़िल थी,<br/>
+
+      मेहनत कर, और खुद को, खुद का ख़ुदा बना के देख।<br/><br/>
+
+      © 2025 <a href="https://mr-ravin.github.io" target="_blank" rel="noopener noreferrer">Ravin Kumar</a>
+      </p>
+      <p align="justify">
+     <b><i>Available on</i></b>:
+     
+     <b> 1. </b> <a target="_blank" rel="noopener noreferrer" href="https://mr-ravin.github.io/Aakhiri_Himmat.pdf">Github</a>
+     <b> 2. </b> <a target="_blank" rel="noopener noreferrer" href="https://archive.org/details/Aakhiri-Himmat">Archive.org</a>
+     <b> 3. </b><a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ch.ravinkumar/%E0%A4%86%E0%A4%96%E0%A4%BC%E0%A4%BF%E0%A4%B0%E0%A5%80-%E0%A4%B9%E0%A4%BF%E0%A4%AE%E0%A5%8D%E0%A4%AE%E0%A4%A4-2a4052e8b454">Medium</a>
+     </p>
+
+    </li>
+    </ol>
+    
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
